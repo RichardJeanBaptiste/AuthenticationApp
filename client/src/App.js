4310925ca1edd729b92f83f487a4cd2017c34a7b
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-10-17 21:52:22 -0400

    wqrwqnicqw

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,3 @@
-/**
- *  TODO
- *  - Create Breakpoints for mobile login/reg
- *  - Add Login Page
- *  - Create Personal Info
- *  - Create Personal Info mobile
- */
-
-
 import { React } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Register from './components/Register';
@@ -26,7 +17,7 @@ function App() {
           <Route exact path="/">
             <Register/>
           </Route>
-          <Route path="/login">
+          <Route exact path="/login">
             <Login/>
           </Route>
           <Route path="/profile/:id">
